ee7b82a612ce5206585e04869ba6f2edda521bb9
OperavonderVollmer <159807320+OperavonderVollmer@users.noreply.github.com>
2025-03-28 04:37:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "pyttsx3",
         "sounddevice",
         "scipy",
-        "OperaPowerRelay @ git+https://github.com/OperavonderVollmer/OperaPowerRelay.git@v1.1.5"
+        "OperaPowerRelay @ git+https://github.com/OperavonderVollmer/OperaPowerRelay.git"
     ],
     python_requires=">=3.7",
     author="Opera von der Vollmer",
